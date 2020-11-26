42dd61f463b3d66e6785d7765027a73df9f73894
Justin Frebault <justin.frebault@gmail.com>
2020-11-26 17:36:03 +0200

    sequence

--- src/main/scala/app/Train.scala
+++ src/main/scala/app/Train.scala
@@ -1,5 +1,7 @@
 package app
 
-case class Train(kind: String, number: Int, schedule: Seq[Station]) {
+case class Train(kind: String, number: Int, schedule: Seq[(Time, Station)]) {
   require(schedule.length > 1, "Schedule must have at least 2 elements")
+
+  val stations: Seq[Station] = schedule.map(_._2)
 }
--- src/test/scala/app/TrainSpec.scala
+++ src/test/scala/app/TrainSpec.scala
@@ -4,11 +4,12 @@ import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class TrainSpec extends AnyFlatSpec with Matchers {
-  "Train" should "be instantiable" in {
-    Train("TGV", 12, Seq(Station("Paris"), Station("London")))
-  }
-
   "Train" should "have at least 2 stations in the schedule" in {
     an[IllegalArgumentException] should be thrownBy Train("TGV", 12, Seq())
+  }
+
+  "Train.stations" should "be the Seq of stations in the schedule" in {
+    val train = Train("TGV", 12, Seq((Time(), Station("Paris")), (Time(2, 10), Station("London"))))
+    train.stations shouldBe Seq(Station("Paris"), Station("London"))
   }
 }

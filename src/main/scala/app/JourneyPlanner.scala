932926785fae0561e654faf023e3ff848d6c0093
Justin Frebault <justin.frebault@gmail.com>
2020-11-27 08:12:20 +0200

    short trip

--- src/main/scala/app/JourneyPlanner.scala
+++ src/main/scala/app/JourneyPlanner.scala
@@ -12,4 +12,13 @@ class JourneyPlanner(trains: Set[Train]) {
       train <- trains
       timeAndStation <- train.schedule if timeAndStation._2 == station
     } yield (timeAndStation._1, train)
+
+  def isShortTrip(from: Station, to: Station): Boolean = {
+    trains.exists(_.stations.dropWhile(_ != from) match {
+      case `from` +: `to` +: _ => true
+      case `from` +: _ +: `to` +: _ => true
+      case _ => false
+    })
+  }
+
 }
--- src/test/scala/app/JourneyPlannerSpec.scala
+++ src/test/scala/app/JourneyPlannerSpec.scala
@@ -30,4 +30,31 @@ class JourneyPlannerSpec extends AnyFlatSpec with Matchers {
 
     journeyPlanner.stopsAt(Station("Paris")) shouldBe Set((Time(), train1), (Time(2, 10),train2))
   }
+
+  "isShortTrip" should "return true if there exists a Train in trains where stations contain from and to with 0 Station in between" in {
+    val bavarianTrain = BavarianRegional(12)
+    val train1 = Train(bavarianTrain, Seq((Time(), Station("Paris")), (Time(2, 10), Station("London"))))
+    val train2 = Train(bavarianTrain, Seq((Time(), Station("Athens")), (Time(2, 10), Station("Paris"))))
+    val journeyPlanner = new JourneyPlanner(Set(train1, train2))
+
+    journeyPlanner.isShortTrip(Station("Paris"), Station("London")) shouldBe true
+  }
+
+  it should "return true if there exists a Train in trains where stations contain from and to with one other Station in between" in {
+    val bavarianTrain = BavarianRegional(12)
+    val train1 = Train(bavarianTrain, Seq((Time(), Station("Paris")), (Time(2, 10), Station("London")), (Time(3, 10), Station("Oxford"))))
+    val train2 = Train(bavarianTrain, Seq((Time(), Station("Athens")), (Time(2, 10), Station("Paris"))))
+    val journeyPlanner = new JourneyPlanner(Set(train1, train2))
+
+    journeyPlanner.isShortTrip(Station("Paris"), Station("Oxford")) shouldBe true
+  }
+
+  it should "return false if there does not exist a Train in trains where stations contain from and to with at most one other Station in between" in {
+    val bavarianTrain = BavarianRegional(12)
+    val train1 = Train(bavarianTrain, Seq((Time(), Station("Paris")), (Time(2, 10), Station("London")), (Time(3, 10), Station("Oxford"))))
+    val train2 = Train(bavarianTrain, Seq((Time(), Station("Athens")), (Time(2, 10), Station("Paris"))))
+    val journeyPlanner = new JourneyPlanner(Set(train1, train2))
+
+    journeyPlanner.isShortTrip(Station("Paris"), Station("Athens")) shouldBe false
+  }
 }
